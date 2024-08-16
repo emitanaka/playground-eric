26a9905e3c34677453950c5244368c25941a8414
Emi Tanaka <dr.emi.tanaka@gmail.com>
2024-08-17 09:27:19 +1000

    Initial commit

---
+++ app.R
@@ -0,0 +1,53 @@
+library(shiny)
+
+# For download of csv files to work for shinylive
+# https://stackoverflow.com/questions/78344439/shinylive-wont-allow-downloading-data-tables-as-csv-files
+downloadButton <- function(...) {
+  tag <- downloadButton(...)
+  tag$attribs$download <- NULL
+  tag
+}
+
+ui <- fluidPage(
+
+  titlePanel("Hi there!"),
+
+  sidebarLayout(
+
+    sidebarPanel(
+
+      shiny::numericInput(inputId = "uid",
+                          label = "Enter your University ID without any u prefix (e.g. 30000000)",
+                          value = 30000000)
+
+    ),
+
+    mainPanel(
+      downloadButton("download", "Download the data below."),
+      br(), br(),
+      dataTableOutput("data")
+    )
+  )
+)
+
+server <- function(input, output) {
+  simdata <- reactive({
+    set.seed(input$uid)
+    n <- 1000
+    x <- runif(n, -1, 1)
+    y <- x + rnorm(n)
+    data.frame(y, x)
+  })
+  output$data <- renderDataTable(simdata())
+  output$download <- downloadHandler(
+    filename = function() {
+      paste0("data-", input$uid, ".csv")
+    },
+    content = function(file) {
+      write.csv(simdata(), file)
+    }
+  )
+
+}
+
+shinyApp(ui = ui, server = server)
