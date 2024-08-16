86ef5a020123c2ca8336985d9306f7ee4d7f4546
Emi Tanaka <dr.emi.tanaka@gmail.com>
2024-08-17 09:42:57 +1000

    add download button

--- app.R
+++ app.R
@@ -2,11 +2,11 @@ library(shiny)
 
 # For download of csv files to work for shinylive
 # https://stackoverflow.com/questions/78344439/shinylive-wont-allow-downloading-data-tables-as-csv-files
-downloadButton <- function(...) {
-  tag <- shiny::downloadButton(...)
-  tag$attribs$download <- NULL
-  tag
-}
+# downloadButton <- function(...) {
+#   tag <- shiny::downloadButton(...)
+#   tag$attribs$download <- NULL
+#   tag
+# }
 
 ui <- fluidPage(
 
@@ -22,7 +22,7 @@ ui <- fluidPage(
     ),
 
     mainPanel(
-      #downloadButton("download", "Download the data below."),
+      downloadButton("download", "Download the data below."),
       br(), br(),
       dataTableOutput("data")
     )
